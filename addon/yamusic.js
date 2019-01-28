12c97bf69f9a3e4cbffdfb28f49d7e2e082e6e16
GoncharovPI <hacpatb@inbox.ru>
2019-01-28 16:35:27 +0500

    1.5.1 Добавил регулировку громкости

--- addon/yamusic.js
+++ addon/yamusic.js
@@ -1,64 +1,12 @@
-//configuration of the observer:
-let config = { attributes: true, childList: true, characterData: true };
-//title observer
-let title = document.querySelector('head > title');
-let titleObserver = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        browser.runtime.sendMessage({
-            action: 'setTitle',
-            title: mutation.target.innerText
-        })
-            .then(handleResponse, onError);
-    });
-});
-titleObserver.observe(title, config);
-
-//button observer
-let butTarget = document.querySelector('.player-controls__btn_play');
-let butObserver1 = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        if (mutation.attributeName == 'class') {
-            let isPlayed = mutation.target.className.split(' ').some((i) => {
-                return i.search('pause') != -1;
-            });
-            browser.runtime.sendMessage({
-                action: 'setPlay',
-                isPlayed: isPlayed
-            })
-                .then(handleResponse, onError);
-        }
-
-    });
-});
-butObserver1.observe(butTarget, config);
-
-//volume observer
-let volumeTarget = document.querySelector('.volume__icon');
-let volumeObserver1 = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        if (mutation.attributeName == 'class') {
-            let isMute = mutation.target.className.split(' ').some((i) => {
-                return i.search('mute') != -1;
-            });
-            browser.runtime.sendMessage({
-                action: 'setMute',
-                isMute: isMute
-            })
-                .then(handleResponse, onError);
-        }
-
-    });
-});
-volumeObserver1.observe(volumeTarget, config);
-
-function getState() {
+let sendState = () => {
     let api = window.wrappedJSObject.externalAPI
-    state = {
+    let state = {
         ...api.getCurrentTrack(),
         ...api.getControls(),
         isPlaying: api.isPlaying(),
         volume: api.getVolume() || 0
     }
+    browser.runtime.sendMessage({ state })
 }
 
 browser.runtime.onMessage.addListener(request => {
@@ -87,32 +35,39 @@ browser.runtime.onMessage.addListener(request => {
                 break;
             case 'volume-up':
                 api.setVolume(api.getVolume() + 0.1 > 1 ? 1 : api.getVolume() + 0.1);
-                //sendPlayerState()
                 break;
             case 'volume-down':
                 api.setVolume(api.getVolume() - 0.1 < 0 ? 0 : api.getVolume() - 0.1);
-                //sendPlayerState()
-                break;
-            case 'GET_PLAYER_STATE':
-                //sendPlayerState()
                 break;
             default:
                 break;
         }
     }
-})
+});
 
-function handleResponse(message) {
-    console.log(`[YaPlay] Message from background: ${message.response}`);
-}
+(function initObserver() {
+    //window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    let config = {
+        attributes: true
+    };
 
-function onError(error) {
-    console.log(`[YaPlay] error: ${error}`);
-}
+    /* Кнопки */
+    let target = document.querySelector('body')
+    let observer = new MutationObserver(mutation => {
+        if (mutation[0].attributeName === 'data-unity-state' ||
+            mutation[0].attributeName === 'data-unity-supports') {
+            sendState();
+        }
+    })
+    observer.observe(target, config);
 
-
-
-
-// later, you can stop observing
-//observer.disconnect();
+    /* Громкость */
+    let volumeTarget = document.querySelector('.volume__icon');
+    let volumeObserver = new MutationObserver(mutation => {
+        if (mutation[0].attributeName == 'class') {
+            sendState()
+        }
+    });
+    volumeObserver.observe(volumeTarget, config);
+})()
 
